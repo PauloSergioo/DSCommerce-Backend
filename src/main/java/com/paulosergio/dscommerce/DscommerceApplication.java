2cb8916c7f9b1beaacab1868a237c90a32c5c1bf
Paulo Sergio <paulosergioilha38@gmail.com>
2023-03-09 14:18:19 -0300

    OAuth2 JWT checklist

--- src/main/java/com/paulosergio/dscommerce/DscommerceApplication.java
+++ src/main/java/com/paulosergio/dscommerce/DscommerceApplication.java
@@ -1,14 +1,20 @@
 package com.paulosergio.dscommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DscommerceApplication {
+public class DscommerceApplication{
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(DscommerceApplication.class, args);
 	}
-
 }
