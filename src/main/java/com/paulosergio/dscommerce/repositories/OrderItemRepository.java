27b06ee07e0846e1713b29ec1d25c4706943c26a
Paulo Sergio <paulosergioilha38@gmail.com>
2023-03-10 18:23:42 -0300

    Saving Order

---
+++ src/main/java/com/paulosergio/dscommerce/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.paulosergio.dscommerce.repositories;
+
+import com.paulosergio.dscommerce.entities.Order;
+import com.paulosergio.dscommerce.entities.OrderItem;
+import com.paulosergio.dscommerce.entities.OrderItemPK;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
+
+}
--- src/main/java/com/paulosergio/dscommerce/services/OrderService.java
+++ src/main/java/com/paulosergio/dscommerce/services/OrderService.java
@@ -2,9 +2,10 @@ package com.paulosergio.dscommerce.services;
 
 
 import com.paulosergio.dscommerce.dto.OrderDTO;
+import com.paulosergio.dscommerce.dto.OrderItemDTO;
 import com.paulosergio.dscommerce.dto.ProductDTO;
-import com.paulosergio.dscommerce.entities.Order;
-import com.paulosergio.dscommerce.entities.Product;
+import com.paulosergio.dscommerce.entities.*;
+import com.paulosergio.dscommerce.repositories.OrderItemRepository;
 import com.paulosergio.dscommerce.repositories.OrderRepository;
 import com.paulosergio.dscommerce.repositories.ProductRepository;
 import com.paulosergio.dscommerce.services.exceptions.ResourceNotFoundException;
@@ -12,16 +13,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+
 @Service
 public class OrderService {
 
     @Autowired
     private OrderRepository repository;
 
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private UserService userService;
+
     @Transactional(readOnly = true)
     public OrderDTO findById(Long id) {
-        Order order = repository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException("Recurso não encontrado"));
-        return  new OrderDTO(order);
+        Order order = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
+        return new OrderDTO(order);
+    }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO dto) {
+        Order order = new Order();
+
+        order.setMoment(Instant.now());
+        order.setStatus(OrderStatus.WAITING_PAYMENT);
+        User user = userService.authenticated();
+        order.setClient(user);
+
+        for (OrderItemDTO itemDTO : dto.getItems()) {
+            Product product = productRepository.getReferenceById(itemDTO.getProductId());
+            OrderItem item = new OrderItem(order, product, itemDTO.getQuantity(), product.getPrice());
+            order.getItems().add(item);
+        }
+        repository.save(order);
+        orderItemRepository.saveAll(order.getItems());
+
+        return new OrderDTO(order);
     }
 }
